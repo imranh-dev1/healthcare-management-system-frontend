f43ddea2bdddf1f88b1cacaf177fd19e2cbb9fac
Imran Hossain <imranhossen0586@gmail.com>
2026-09-16 20:46:41 +0600

    feat: add user logout and get user details functionality with hooks

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -2,4 +2,10 @@ import apiClint from "@/lib/apiClint";
 
 export function userLogin(payload: { email: string, password: string }) {
     return apiClint("/auth/login", { method: "POST", body: payload })
+}
+export function userLogOut() {
+    return apiClint("/auth/logout", { method: "POST" })
+}
+export function getMe() {
+    return apiClint("/auth/me", { method: "GET" })
 } 
--- src/hooks/auth.hook.ts
+++ src/hooks/auth.hook.ts
@@ -1,8 +1,21 @@
-import { userLogin } from "@/api";
-import { useMutation } from "@tanstack/react-query";
+import { getMe, userLogin, userLogOut } from "@/api";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export function useLogin() {
     return useMutation({
         mutationFn: userLogin
+    })
+}
+
+export function useLogOut() {
+    return useMutation({
+        mutationFn: userLogOut
+    })
+}
+
+export function useGetMe() {
+    return useQuery({
+        queryKey: ["user"],
+        queryFn: getMe,
     })
 } 
