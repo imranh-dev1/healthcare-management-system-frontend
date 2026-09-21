5e33075f5f6b805710085a29bfbe819c7250a32c
Imran Hossain <imranhossen0586@gmail.com>
2026-09-21 13:31:53 +0600

    feat: add padding to TabsTrigger in DoctorApprovalTabs component

--- src/components/module/doctor-approval/doctor-approval-tabs.tsx
+++ src/components/module/doctor-approval/doctor-approval-tabs.tsx
@@ -35,7 +35,7 @@ export default function DoctorApprovalTabs() {
                 >
                     <TabsList>
                         {doctorActiveStatus.map(([status, label]) => (
-                            <TabsTrigger
+                            <TabsTrigger className="px-4"
                                 key={status}
                                 value={status}
                             >
